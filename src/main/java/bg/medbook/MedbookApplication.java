529040333372a12600a7464bf2c768fb4670f58d
Darina Krasimirova <darina11krasimirova@gmail.com>
2022-08-15 22:57:15 +0300

    Initial commit

---
+++ src/main/java/bg/medbook/MedbookApplication.java
@@ -0,0 +1,12 @@
+package bg.medbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedbookApplication.class, args);
+    }
+}
---
+++ src/test/java/bg/medbook/MedbookApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.medbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
